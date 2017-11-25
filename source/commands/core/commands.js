e8924e41aeb4f7f768ac855b09074128bde69e5e
HyperCoder <bryan.pikaardjr@outlook.com>
2017-11-25 17:12:24 -0500

    Embed color defaults

--- source/commands/core/commands.js
+++ source/commands/core/commands.js
@@ -12,7 +12,6 @@ module.exports = class extends Command {
 
     async execute(message, permissionLevel) {
         message.buildEmbed()
-            .setColor(0x52C7CE)
             .setTitle(`${this.client.user.username}`)
             .addField("» Commands", this.client.commands.map(c => c.name).join(', '), true)
             .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
--- source/utility/Extenders.js
+++ source/utility/Extenders.js
@@ -2,6 +2,8 @@ const { MessageEmbed, TextChannel, DMChannel, User, Message } = require("discord
 
 MessageEmbed.prototype.send = function(content) {
     if (!this.sendToChannel || !(this.sendToChannel instanceof TextChannel || this.sendToChannel instanceof User || this.sendToChannel instanceof DMChannel)) return Promise.reject("Embed not created in a channel");
+    if (!this.color) this.setColor(this.client.config.embedColor || 0x52C7CE);
+    
     return this.sendToChannel.send(content || "", { embed: this });
 };
 
